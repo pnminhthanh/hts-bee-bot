6e90db19fc4c0c8ea7f92472beed3f6e9c4eb995
pnminhthanh <mix.kris97@gmail.com>
2024-12-03 23:31:40 +0700

    feat: add error handling for missing environment variables and enhance logging in bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,12 @@ const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+if (!token) {
+  console.error(
+    "Error: TELEGRAM_BOT_TOKEN is not set in environment variables."
+  );
+  process.exit(1);
+}
 const bot = new TelegramBot(token);
 
 const app = express();
@@ -9,6 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.post(`/bot${token}`, (req, res) => {
+  console.log("Received request:", req.body);
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
@@ -19,21 +26,36 @@ app.listen(PORT, () => {
 });
 
 const webhookUrl = `https://hts-bee-bot.onrender.com/bot${token}`;
-bot.setWebHook(webhookUrl);
+bot
+  .setWebHook(webhookUrl)
+  .then(() => {
+    console.log(`Webhook set to ${webhookUrl}`);
+  })
+  .catch((error) => {
+    console.error("Error setting webhook:", error);
+  });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    "Xin chào! Em là Bee Bot của The Honeycomb Riches Squad."
-  );
+  console.log(`/start command received from chatId: ${chatId}`);
+  bot
+    .sendMessage(
+      chatId,
+      "Xin chào! Em là Bee Bot của The Honeycomb Riches Squad."
+    )
+    .catch((error) => {
+      console.error("Error sending /start message:", error);
+    });
 });
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
+  console.log("Received message:", msg);
 
   if (text && text.includes(`@${msg.bot.username}`)) {
-    bot.sendMessage(chatId, `Bạn vừa nhắc tôi: "${text}"`);
+    bot.sendMessage(chatId, `Bạn vừa nhắc tôi: "${text}"`).catch((error) => {
+      console.error("Error sending mention response:", error);
+    });
   }
 });
